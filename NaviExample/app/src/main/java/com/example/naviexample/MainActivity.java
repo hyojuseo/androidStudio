f6c85f99279cd43272f6662fae49892c00eb427b
seohyoju <kinnylovemy@naver.com>
2021-11-01 21:03:30 +0900

    onNavigationItemSelected가 오버라이드 안됨

--- NaviExample/app/src/main/java/com/example/naviexample/MainActivity.java
+++ NaviExample/app/src/main/java/com/example/naviexample/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.naviexample;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -28,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        //오른쪽우측의 메일아이콘형태를 관리하는 구문
         setSupportActionBar(binding.appBarMain.toolbar);
         binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
         DrawerLayout drawer = binding.drawerLayout; //메뉴바
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
@@ -49,11 +54,42 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(navigationView, navController);
     }
 
+    //hyoju
+    //뒤로가기 버튼을 누르면 아래 메소드가 실행
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public void onBackPressed(){   
+        //뒤로가기를 눌렀을때.
+        DrawerLayout drawer = (DrawerLayout)findViewById(R.id.drawer_layout);
+        //DrawerLayout이 열려있다면
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            //닫아주기
+            drawer.closeDrawer(GravityCompat.START);
+        } else{
+            //DrawerLayout이 닫혀있다면
+            //뒤로가기
+            super.onBackPressed();
+        }
+    }
+
+    //만들어진 템플릿 가져오기
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) { //앱을 시작할때 옵션메뉴를 만들어둔다.
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    //hyoju
+    //옵션메뉴를 클릭했을 때.
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        //해당되는
+        if(id == R.id.action_settings){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -61,5 +97,25 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    //hyoju
+    //각각 메뉴버튼을 클릭했을 때 이루어지는 액션을 여기서 실행
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if(id == R.id.nav_home){
+            //empty
+        }else if(id == R.id.nav_gallery){
+            //empty
+        }else if(id == R.id.nav_slideshow){
+            //empty
+        }
+
+        DrawerLayout drawer = (DrawerLayout)findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
